f78839e62f25984d00dbecb7c755b9c878e3023f
Subham <rajshubham199@gmail.com>
2023-12-11 07:23:36 +0530

    some changes made on cards and popup

--- src/components/Pets.jsx
+++ src/components/Pets.jsx
@@ -1,66 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import CardActions from '@mui/material/CardActions';
-import Button from '@mui/material/Button';
-import petsData from '../pets.json';
-import { makeStyles } from '@mui/styles';
-
+import React, { useState, useEffect } from "react";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import petsData from "../pets.json";
+import { makeStyles } from "@mui/styles";
+import PopupComponent from "./Popup";
 
 const useStyles = makeStyles(() => ({
   cardContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    gap: '20px', 
-    justifyContent: 'space-between', 
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "20px",
+    justifyContent: "space-between",
   },
   card: {
     maxWidth: 345,
-    width: '30%', 
-    marginBottom: '20px',
+    width: "30%",
+    marginBottom: "20px",
   },
 }));
 
 function Pets({ onCardClick }) {
-
   const [pets, setPets] = useState([]);
+  const [selectedPetId, setSelectedPetId] = useState(null);
+  const [popupOpen, setPopupOpen] = useState(false);
   const classes = useStyles();
 
   useEffect(() => {
     setPets(petsData.pets);
-  },[]);
+  }, []);
 
-  const openDetails =(petId) => 
-  {
-    const selectedPet = pets.find((pet) => pet.id === petId);
-    onCardClick(selectedPet)
-  }
+  const openDetails = (petId) => {
+    if (petId === selectedPetId && popupOpen) {
+      setPopupOpen(false); // Toggle the popupOpen state
+    } else {
+      setSelectedPetId(petId);
+      setPopupOpen(true);
+    }
+  };
+
+  const closePopup = () => {
+    setPopupOpen(false);
+  };
+
+  const selectedPet = pets.find((pet) => pet.id === selectedPetId);
 
   return (
     <div className={classes.cardContainer}>
       {pets.map((pet) => (
-        <Card key={pet.id} className={classes.card} onClick={()=>openDetails(pet.id)} >
+        <Card
+          key={pet.id}
+          className={classes.card}
+          onClick={() => openDetails(pet.id)}
+        >
           <CardMedia sx={{ height: 250 }} image={pet.image} title={pet.name} />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {pet.name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Category: {pet.Category}
-            </Typography>
+          <CardContent
+            style={{ display: "flex", justifyContent: "space-between" }}
+          >
+            <span>{pet.name}</span>
+            <span>{pet.Category}</span>
           </CardContent>
-          <CardActions>
-            <Button size="small" onClick={()=>openDetails(pet.id)}>Share</Button>
-            <Button size="small" onClick={()=>openDetails(pet.id)}>Learn More</Button>
+
+          <CardActions style={{ justifyContent: 'flex-end' }}>
+            <Button size="small" onClick={() => openDetails(pet.id)}>
+              More about
+            </Button>
           </CardActions>
         </Card>
       ))}
+      {popupOpen && selectedPet && (
+        <PopupComponent
+          pet={selectedPet}
+          open={popupOpen}
+          onClose={closePopup}
+        />
+      )}
     </div>
   );
 }
 
 export default Pets;
-
 
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -8,13 +8,16 @@ import Modal from "@mui/material/Modal";
 import Fade from "@mui/material/Fade";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import SendIcon from '@mui/icons-material/Send';
+import PetsIcon from '@mui/icons-material/Pets';
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: 450,
+  height: 550,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
@@ -23,10 +26,8 @@ const style = {
 
 export default function TransitionsModal({ pet }) {
   const [open, setOpen] = React.useState(false);
-  const [selectedPet, setSelectedPet] = useState(null);
 
-  const handleOpen = (pet) => {
-    setSelectedPet(pet);
+  const handleOpen = () => {
     setOpen(true);
   };
 
@@ -42,7 +43,7 @@ export default function TransitionsModal({ pet }) {
 
   return (
     <div>
-      <Button onClick={() => handleOpen(selectedPet)}>Open modal</Button>
+      <Button onClick={handleOpen}>Open modal</Button>
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
@@ -58,34 +59,84 @@ export default function TransitionsModal({ pet }) {
           <Box sx={style}>
             {pet && (
               <>
-                <Typography
-                  id="transition-modal-title"
-                  variant="h6"
-                  component="h2"
-                >
-                  <Card>
-                    <CardMedia
-                      sx={{ height: 250 }}
-                      image={pet.image}
-                      title={pet.name}
-                    />
-                    <CardContent>
+                <Card>
+                  <CardMedia
+                    sx={{ height: 250 }}
+                    image={pet.image}
+                    title={pet.name}
+                  />
+                  <CardContent
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "space-between",
+                      height: "100%",
+                    }}
+                  >
+                    <div>
                       <Typography gutterBottom variant="h5" component="div">
                         {pet.name}
                       </Typography>
-                      <Typography gutterBottom variant="h7" component="div">
-                      {pet.Category}
+                      <Typography gutterBottom variant="body2" component="div">
+                        {pet.Category}
                       </Typography>
-                    </CardContent>
-                  </Card>
-                </Typography>
-                <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                  
-                  {pet.price}
-                  {pet.breed}
-                  {pet.age}
-                  {pet.gender}
-                </Typography>
+                    </div>
+                    
+                    <div>
+                      <Typography variant="body2" component="div">
+                        <div
+                          style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            marginBottom: "5px",
+                          }}
+                        >
+                          <div style={{ width: "90px" }}>Price:</div>
+                          <div>{pet.price}</div>
+                        </div>
+                        <div
+                          style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            marginBottom: "5px",
+                          }}
+                        >
+                          <div style={{ width: "90px" }}>Breed:</div>
+                          <div>{pet.breed}</div>
+                        </div>
+                        <div
+                          style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            marginBottom: "5px",
+                          }}
+                        >
+                          <div style={{ width: "90px" }}>Age:</div>
+                          <div>{pet.age}</div>
+                        </div>
+                        <div
+                          style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                          }}
+                        >
+                          <div style={{ width: "90px" }}>Gender:</div>
+                          <div>{pet.gender}</div>
+                        </div>
+                        
+                      </Typography>
+                    </div>
+                    <div>
+                      <Typography>
+                        <div className="container-popup">
+                          <div className="submit-popup" onClick={handleClose}> More <PetsIcon sx={{ fontSize: 20, marginLeft: '5px' }}/></div>
+                        
+                          <div className="submit-popup">Enquiry <SendIcon sx={{ fontSize: 20, marginLeft: '5px' }}/></div>
+                        </div>
+                      </Typography>
+                    </div>
+                  </CardContent>
+                </Card>
               </>
             )}
           </Box>
